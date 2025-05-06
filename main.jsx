2f639ab50a1dad83e7a16b426a6bb4f5fe555739
Dane McManus <43182252+mcmanusd1@users.noreply.github.com>
2025-05-06 17:33:00 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FlashcardGame from "./FlashcardGame";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FlashcardGame />
+  </React.StrictMode>
+);
